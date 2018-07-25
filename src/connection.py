5ed94a8c3a9d47a4f2fff9713964dd007dc3a338
mlkersten <martin.kersten@cwi.nl>
2018-07-25 21:41:37 +0200

    Extra information

--- src/connection.py
+++ src/connection.py
@@ -10,6 +10,7 @@ The tasks are obtained from a webservice. It does not itself provide a web servi
 """
 import requests
 import json
+import os
 
 
 class Connection:
@@ -21,6 +22,9 @@ class Connection:
     dbms = None
     version = None
     tasks = None
+    preload = None
+    postload = None
+    memory = -1
 
     def __init__(self, target,):
         """
@@ -35,7 +39,11 @@ class Connection:
         self.server = target['server']
         self.user = target['user']
         # construct password hash
-
+        try:
+            mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+            self.memory = mem_bytes / (1024. ** 3)
+        except:
+            pass
         if 'input' in target and target['input']:
             # read the input file with experiment records and process them one by one
             with open(target['input'], 'r') as f:
@@ -47,11 +55,13 @@ class Connection:
         debug = target.getboolean('debug')
 
         # read from the batch file first
-        if self.tasks:
+        if self.tasks and 'input' in target:
             t = self.tasks[0]
             self.tasks.remove(0)
             return t
         if self.tasks == []:
+            if debug:
+                print('no elements in local task queue')
             return None
 
         if 'db' in target:
@@ -68,7 +78,7 @@ class Connection:
             experiment = '*'
 
         endpoint = 'http://' + self.server + '/get_work'
-        args = {'user': self.user, 'host': self.host, 'dbms': self.dbms, 'version':self.version, 'db': db,
+        args = {'user': self.user, 'host': self.host, 'dbms': self.dbms, 'version': self.version, 'db': db,
                 'project': project, 'experiment': experiment, 'passwordhash': 0}
         if target.getboolean('extras'):
             # also ask for the template and binding table
@@ -80,7 +90,8 @@ class Connection:
                 print('Endpoint', endpoint)
                 print('Requesting', json.dumps(args, sort_keys=True, indent=4))
             response = requests.get(endpoint,  json=args, timeout=20)
-        except:
+        except requests.exceptions.RequestException as e:
+            print('REQUESTS exception', e)
             if debug:
                 print('WEB SERVER RESPONSE ', response)
             return None
@@ -92,7 +103,7 @@ class Connection:
             print('No tasks available for the target section', json.dumps(args, sort_keys=True, indent=4))
         if debug:
             print('Task received:', task)
-
+        self.preload = [ "%.3f" % v for v in list(os.getloadavg())]
         return task
 
     def put_work(self, task, results, debug):
@@ -100,12 +111,16 @@ class Connection:
             if debug:
                 print('Missing result object')
             return None
-
+        try:
+            self.postload = [ "%.3f" % v for v in list(os.getloadavg())]
+        except os.error:
+            pass
         endpoint = 'http://' + self.server + '/put_work'
         u = {'exp': task['exp'], 'tag': task['tag'], 'ptag': task['ptag'],
              'usr': self.user, 'host': self.host, 'dbms': self.dbms, 'version': self.version,
              'db': task['db'], 'project': task['project'], 'experiment': task['experiment'],
              'query': task['query'].replace("'", "''"),
+             'cpucount': os.cpu_count(), 'cpuload': str(self.preload + self.postload).replace("'",""), 'ram': self.memory,
              }
 
         results.update(u)
@@ -115,7 +130,8 @@ class Connection:
                 print('sending', json.dumps(results, sort_keys=True, indent=4))
             response = requests.post(endpoint, json=results)
             return response.status_code == 200
-        except:
+        except requests.exceptions.RequestException as e:
+            print('REQUESTS exception', e)
             print('Failed to post to ', endpoint)
         if debug:
             print('Sent task result', response)
