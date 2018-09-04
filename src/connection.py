6cd375b3e158b4e7401dcba27f909982487bb2a7
mlkersten <martin.kersten@cwi.nl>
2018-09-04 09:04:07 +0200

    Re-organize structure

--- src/connection.py
+++ src/connection.py
@@ -63,7 +63,7 @@ class Connection:
 
         endpoint = 'http://' + self.server + '/get_work'
         args = {'key': self.key, 'host': self.host, 'dbms': self.dbms, 'version': self.version, 'db': db,
-                'project': project, 'experiment': experiment, 'passwordhash': 0, 'branch':'master'}
+                'owner':'anon','project': project, 'experiment': experiment, 'passwordhash': 0, 'branch':'master'}
         if target.getboolean('extras'):
             # also ask for the template and binding table
             args.update({'extras': 'yes'})
@@ -102,7 +102,7 @@ class Connection:
         endpoint = 'http://' + self.server + '/put_work'
         u = {'run': task['run'], 'tag': task['tag'], 'ptag': task['ptag'],'key': self.key,
              'host': self.host, 'dbms': self.dbms, 'version': self.version,
-             'db': task['db'], 'project': task['project'], 'experiment': task['experiment'],
+             'db': task['db'], 'owner': task['owner'], 'project': task['project'], 'experiment': task['experiment'],
              'query': task['query'].replace("'", "''"), 'branch': task['branch'],
              'cpucount': os.cpu_count(), 'cpuload': str(self.preload + self.postload).replace("'",""),
              'ram': self.memory,
