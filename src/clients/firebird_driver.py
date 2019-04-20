a2086b5e3f59b6f55efe1038b6be167d139dc07d
mlkersten <martin.kersten@cwi.nl>
2019-04-20 23:19:25 +0200

    revamp the interface with Sqalpel

--- src/clients/firebird_driver.py
+++ src/clients/firebird_driver.py
@@ -7,9 +7,19 @@ Copyright 2019- Stichting Sqalpel
 
 Author: M. Kersten
 
-Execute a single query multiple times on the database nicknamed 'dbms'
-and return a list of timings. The first error encountered stops the sequence.
-The result is a dictionary with at least the structure {'times': [...]}
+Execute a single query multiple times on the database nicknamed 'db'
+and return a list of timings. The first error encountered aborts the sequence.
+The result is a list of dictionaries
+run: [{
+    times: [<response time>]
+    chks: [<integer value to represent result (e.g. cnt,  checksum or hash over result set) >]
+    param: {param1:value1, ....}
+    errors: []
+    }]
+
+If parameter value lists are given, we run the query for each element in the product.
+
+Internal metrics, e.g. cpu load, is returned as a JSON structure in 'metrics' column
 """
 
 import re
@@ -22,72 +32,136 @@ import datetime
 import fdb
 import os
 
+
 class FirebirdDriver:
+    conn = None
+    db = None
 
     def __init__(self):
         pass
 
     @staticmethod
-    def run(target):
+    def startserver(db):
+        if FirebirdDriver.conn:
+            # avoid duplicate connection
+            if FirebirdDriver.db == db:
+                return None
+            FirebirdDriver.stopserver()
+        print('Start FirebirdDriver', db)
+        try:
+            FirebirdDriver.conn = fdb.connect(database=db)
+        except (Exception, fdb.DatabaseError()) as msg:
+            print('EXCEPTION ', msg)
+            if FirebirdDriver.conn is not None:
+                FirebirdDriver.close()
+            return [{'error': json.dumps(msg), 'times': [], 'chks': [], 'param': []}]
+        return None
+
+    @staticmethod
+    def stopserver():
+        if not FirebirdDriver.conn:
+            return None
+        print('Stop FirebirdDriver')
+        # to be implemented
+        return None
+
+    @staticmethod
+    def run(task):
         """
-        The number of repetitions is used to derive the best-of value.
-        :param target:
+        :param task:
         :return:
         """
-        db = target['db']
-        socket = target['dbsocket']
-        query = target['query']
-        params = target['params']
-        runlength = int(target['runlength'])
-        timeout = int(target['timeout'])
-        debug = target.getboolean('debug')
-        response = {'error': '', 'times': [], 'cnt': [], 'clock': [], 'extra': []}
-        try:
-            preload = [ "%.3f" % v for v in list(os.getloadavg())]
-        except os.error:
-            preload = 0
-            pass
+        debug = task.getboolean('debug')
+        db = task['db']
+        query = task['query']
+        params = task['params']
+        options = json.loads(task['options'])
+        if 'runlength' in options:
+            runlength = int(options['runlength'])
+        else:
+            runlength = 1
+        print('runs', runlength)
 
-        conn = None
-        try:
-            fdb.connect(database = '/path/database.db', user = 'sysdba', password = 'pass')
-        except (Exception, fdb.DatabaseError) as msg:
-            print('Connection', target['port'], db)
-            print('Exception', msg)
-            if conn:
-                conn.close()
-                print('Database connection closed')
-            return response
+        response = []
+        error = ''
+        msg = FirebirdDriver.startserver(db)
+        if msg:
+            FirebirdDriver.stopserver()
+            return msg
 
-        if debug:
-            nu = time.strftime('%Y-%m-%d %H:%m:%S', time.localtime())
-            print('Run query:', nu, ':',  query)
+        if params:
+            data = [json.loads(params[k]) for k in params.keys()]
+            names = [d for d in params.keys()]
+            gen = itertools.product(*data)
+        else:
+            gen = [[1]]
+            names = ['_ * _']
 
-        for i in range(runlength):
-            try:
-                nu = time.strftime('%Y-%m-%d %H:%m:%S', time.localtime())
-                c= conn.cursor()
-                ms = datetime.datetime.now()
-                c.execute(query)
-                response['answer'] = 'No answer'
-                ms = datetime.datetime.now() - ms
-            except fdb.DatabaseError as msg:
-                # a timeout should also stop the database process involved the hard way
-                print('EXCEPTION ', i,  msg)
-                response['error'] = str(msg).replace("\n", " ").replace("'", "''")
-                return response
-
+        for z in gen:
+            if error != '':
+                break
             if debug:
-                print('response ', proc.stdout.decode('ascii')[:-1])
+                print('Run query:', time.strftime('%Y-%m-%d %H:%m:%S', time.localtime()))
+                print('Parameter:', z)
+                print(query)
 
-            response['times'].append(float(ms.microseconds) / 1000.0)
-            response['cnt'].append(-1)  # not yet collected
-            response['extra'].append([])
-            response['clock'].append(nu)
-        try:
-            postload = [ "%.3f" % v for v in list(os.getloadavg())]
-        except os.error:
-            postload = 0
-            pass
-        response['cpuload'] = str(preload + postload).replace("'", "")
+            args = {}
+            for n, v in zip(names, z):
+                if params:
+                    args.update({n: v})
+            try:
+                preload = [v for v in list(os.getloadavg())]
+            except os.error:
+                preload = 0
+
+            times = []
+            chks = []
+            newquery = query
+            if z:
+                if debug:
+                    print('args:', args)
+                # replace the variables in the query
+                for elm in args.keys():
+                    newquery = re.sub(elm, str(args[elm]), newquery)
+                print('New query', newquery)
+
+            for i in range(runlength):
+                try:
+                    c = FirebirdDriver.conn.cursor()
+
+                    ticks = time.time()
+                    c.execute(newquery)
+                    r = c.fetchone()
+                    if r:
+                        chks.append(int(r[0]))
+                    else:
+                        chks.append('')
+                    times.append(int((time.time() - ticks) * 1000))
+
+                    if debug:
+                        print('ticks[%s]' % i, times[-1])
+                    c.close()
+                except (Exception, pymonetdb.DatabaseError) as msg:
+                    print('EXCEPTION ', msg)
+                    error = str(msg).replace("\n", " ").replace("'", "''")
+                    break
+
+            # wrapup the experimental runs,
+            # The load can be sent as something extra, it is an internal metric
+            try:
+                postload = [v for v in list(os.getloadavg())]
+            except os.error:
+                postload = 0
+
+            res = {'times': times,
+                   'chksum': chks,
+                   'param': args,
+                   'error': error,
+                   'metrics': {'load': preload + postload},
+                   }
+
+            response.append(res)
+        if debug:
+            print('Finished the run')
+        FirebirdDriver.stopserver()
         return response
